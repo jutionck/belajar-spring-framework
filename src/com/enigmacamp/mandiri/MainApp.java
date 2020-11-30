6c509ab2e8a40ff0968e68695fb7a0b58d74e501
Jution Candra Kirana <jutionck@gmail.com>
2020-11-30 16:38:22 +0700

    Spring bean with Ioc

--- src/com/enigmacamp/mandiri/MainApp.java
+++ src/com/enigmacamp/mandiri/MainApp.java
@@ -4,21 +4,9 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        //Object baseBallCoach
-        //Coach baseBallCoach = new BaseBallCoach();
-
-        //Object trackCouch
-        //This is still hardcode (not recommended)
-        //Coach trackCoach = new TrackCoach();
-        //System.out.println(trackCoach.getDailyWorkout());
-
-        //Using spring container with solve -> IoC and DI
         /*
-        XML -> Legacy, but still use this
-        Java Annotation -> Modern
-        Java Source Code -> Modern
-
-
+        Reference : https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#beans-introduction
          */
+
     }
 }
