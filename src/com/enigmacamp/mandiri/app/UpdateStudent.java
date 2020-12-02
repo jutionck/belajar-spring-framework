7ec13cef592be897d26a072285d5407c859eff5e
Jution Candra Kirana <jutionck@gmail.com>
2020-12-02 14:52:03 +0700

    Update object

---
+++ src/com/enigmacamp/mandiri/app/UpdateStudent.java
@@ -0,0 +1,39 @@
+package com.enigmacamp.mandiri.app;
+
+import com.enigmacamp.mandiri.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateStudent {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try (factory) {
+            int studentId = 1;
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Student myStudent = session.get(Student.class, studentId);
+            System.out.println("Update student...");
+            myStudent.setLastName("Candra Kirana");
+            session.getTransaction().commit();
+
+            //Update with createQuery
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            System.out.println("Update with createQuery..");
+            session.createQuery("UPDATE Student set email = 'jutionck@gmail.com' WHERE id = 1").executeUpdate();
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+    }
+}
